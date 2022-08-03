34ba92e941a124c9c288f673870732701e4c3f3e
Narek Ghazaryan <narek.ghazaryan@dataart.com>
2022-08-03 12:42:20 +0400

    adding all validations to one function

--- Sign-up/signup.js
+++ Sign-up/signup.js
@@ -4,16 +4,20 @@ const signupConfirmPassword = document.getElementById('signup-confirm-password')
 const signupButton = document.getElementById('signup-button');
 const database = window.localStorage;
 
-let userCount = 0;
 
 // regex for login (minimum 8 length)
 const checkLoginValidity = /^[A-Za-z][A-Za-z0-9_]{7,29}$/;
+
+
 // regex for password (minimum 8 length, minimum 1 letter)
 const checkPasswordValidity = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+
 // function for checking password and confirm password fields equality
 const checkConfirmation = (a, b) => {
     return a.value === b.value
 }
+
 
 // checking to add only not existing login
 const checkingUserExists = () => {
@@ -24,16 +28,22 @@ const checkingUserExists = () => {
 }
 
 
-
-// adding event listener to sign up button with checks 
-signupButton.addEventListener('click', () => {
+// checking if all validations of sign up have been passed
+const allValidationsPass = () => {
     if (
         checkingUserExists() &&
         checkConfirmation(signupPassword, signupConfirmPassword) &&
         checkLoginValidity.test(signupLogin.value) &&
         checkPasswordValidity.test(signupPassword.value)
     ) {
-        database.setItem(`${signupLogin.value}`, signupLogin.value)
-        userCount++
+        return true
     }
+    return false
+}
+
+
+
+// adding event listener to sign up button with checks 
+signupButton.addEventListener('click', () => {
+     allValidationsPass() ?  database.setItem(`${signupLogin.value}`, signupLogin.value) : null
 })
